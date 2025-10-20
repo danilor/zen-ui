afb1234cf51867706ca2c700da31a4357553d082
Danilo Ramirez <danilram@cisco.com>
2025-10-20 14:38:13 -0600

    Improving the search example in the example application

--- example/src/components/examples/SearchExample.tsx
+++ example/src/components/examples/SearchExample.tsx
@@ -1,38 +1,33 @@
 import { ZenDivider, ZenSearch, ZenText } from 'react-zen-ui';
 
-export default function SearchExample(){
+export default function SearchExample() {
   return (
     <>
       <ZenText type={'h4'} paragraph={true} align={'left'}>
         Search
       </ZenText>
-
       <ZenText type={'p'} paragraph={true} align={'justify'}>
-        Search element built on top of the ZenInput component. It provides additional functionality like
-        debouncing and a clear button.
+        Search element built on top of the ZenInput component. It provides
+        additional functionality like debouncing and a clear button.
       </ZenText>
-
       <ZenDivider type={'primary'} />
-
       <ZenSearch />
-
       <ZenDivider type={'primary'} />
       <ZenSearch type={'secondary'} />
-
       <ZenDivider type={'primary'} />
       <ZenSearch type={'danger'} />
-
       <ZenDivider type={'primary'} />
       <ZenSearch type={'info'} />
-
       <ZenDivider type={'primary'} />
       <ZenSearch type={'warning'} />
-
       <ZenDivider type={'primary'} />
       <ZenSearch type={'success'} />
-
       <ZenDivider type={'primary'} />
-      <ZenSearch type={'primary'} placeholder={'Disabled search'} disabled={true} />
+      <ZenSearch
+        type={'primary'}
+        placeholder={'Disabled search'}
+        disabled={true}
+      />
     </>
   );
 }
