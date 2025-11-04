c9648aba6d6ef919a8330422a80a4d1595f3de65
Danilo Ramirez <danilram@cisco.com>
2025-11-04 15:17:55 -0600

    Adding snack to documentation

--- src/components/widgets/ZenImageSlider.tsx
+++ src/components/widgets/ZenImageSlider.tsx
@@ -113,7 +113,7 @@ export default function SliderScreen() {
 
 
  {/tsx}
- @embed <div data-snack-id="@daniloramirezcr/c906b8" data-snack-platform="web" data-snack-preview="true" data-snack-theme="light" style="overflow:hidden;background:#fbfcfd;border:1px solid var(--color-border);border-radius:4px;height:505px;width:100%"></div><script async src="https://snack.expo.dev/embed.js"></script>
+ @snack @daniloramirezcr/c906b8
  */
 export default function ZenImageSlider({
   images,
